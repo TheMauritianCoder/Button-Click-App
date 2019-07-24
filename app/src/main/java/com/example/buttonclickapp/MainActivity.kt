e76dd0d0407159165c31ff260c9baa2d3e9fe3f9
Ravi Kowlessur <ravi.kowlessur@gmail.com>
2019-07-24 16:07:04 +0400

    Clearing Texts on Button Click

--- app/src/main/java/com/example/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -10,33 +10,30 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private var userInput : EditText? = null
-    private var button: Button?= null
     private var textView: TextView?= null
-    private var numberOfTimesClicked = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById<EditText>(R.id.editText)
-        button = findViewById<Button>(R.id.button)
+        val userInput: EditText = findViewById<EditText>(R.id.editText)
+        val button: Button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
         textView?.text = ""
         textView?.movementMethod = ScrollingMovementMethod() // Allows scrolling in textView
 
-        // Long Version
-        button?.setOnClickListener(object : View.OnClickListener{
+
+        userInput.text.clear()
+
+        button.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                numberOfTimesClicked += 1
-                textView?.append("The Button got tapped $numberOfTimesClicked time")
-                if(numberOfTimesClicked > 1){
-                    textView?.append("s")
-                }
+                textView?.append(userInput.text)
                 textView?.append("\n")
+
+                // Clear input on click
+                userInput.text.clear()
+                //userInput.setText("")
             }
         })
-
-        // Short Version
     }
 }
